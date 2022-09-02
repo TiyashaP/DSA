037b159bc22b401b7c7b01dfd03174a2b045c43d
tiyashap <tiyasha01@gmail.com>
2022-09-02 20:30:45 +0200

    spiralprintsolution

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 1. **2 d arrays:**
 [WavePrint Problem](https://www.codingninjas.com/codestudio/problems/print-like-a-wave_893268?utm_source=youtube&utm_medium=affiliate&utm_campaign=love_babbar_6)
-[Solution:](https://github.com/TiyashaP/DSA/blob/main/twodarray/WavePrint.java)
+[Solution](https://github.com/TiyashaP/DSA/blob/main/twodarray/WavePrint.java)
+Approach: In this pblm every even column is printed from top to bottom(i.e. minRow to maxrow)
+and every odd row is printed from bottom to top.
+
+[Spiral Matrix](https://leetcode.com/problems/spiral-matrix/)
+[Solution](https://github.com/TiyashaP/DSA/blob/main/twodarray/SpiralMatrix.java)
+![img.png](img.png)
+Approach: Steps:
+We create a list equal to the size of the 2d array
+We initialise startRow,startColumn,endRow,endColumn
+Till we achieve desired count we repeat following steps
+Add/print the start row,update start row after print/add,update count
+Add/print the endColumn,update endColumn after print/add,update count
+Add/print the endRow ,update endRow after print/add,update count
+Add/print the startColumn,update startColumn after print/add,update count
+
---
+++ twodarray/SpiralMatrix.java
@@ -0,0 +1,48 @@
+package twodarray;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class SpiralMatrix {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> spiralList=new ArrayList<>();
+        int rows=matrix.length;
+        int columns=matrix[0].length;
+        int startRow=0;
+        int startColumn=0;
+        int endRow=rows-1;
+        int endColumn=columns-1;
+        int count=0;
+        int total=rows*columns;
+        while(count<total)
+        {
+            for(int i=startColumn;i<=endColumn && count<total;i++)
+            {
+                spiralList.add(matrix[startRow][i]);
+                count++;
+            }
+            startRow++;
+            for(int i=startRow;i<=endRow && count<total;i++)
+            {
+                spiralList.add(matrix[i][endColumn]);
+                count++;
+            }
+            endColumn--;
+            for(int i=endColumn;i>=startColumn && count<total;i--)
+            {
+                spiralList.add(matrix[endRow][i]);
+                count++;
+            }
+            endRow--;
+            for(int i=endRow;i>=startRow && count<total;i--)
+            {
+                spiralList.add(matrix[i][startColumn]);
+                count++;
+            }
+            startColumn++;
+
+        }
+
+        return spiralList;
+    }
+}
