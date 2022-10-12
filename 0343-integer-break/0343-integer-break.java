6a499c2ad372652d44d7c3f1aa72c58f84c5d84e
TiyashaP <35894987+TiyashaP@users.noreply.github.com>
2022-10-12 11:23:11 +0200

    Time: 1 ms (68.98%), Space: 40.2 MB (82.99%) - LeetHub

---
+++ 0343-integer-break/0343-integer-break.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int integerBreak(int n) {
+        int[] dp=new int[n+1];
+        dp[1]=1;
+        return integerBreakRec(n,n,dp);
+        
+    }
+    
+     public static int integerBreakRec(int n,int num,int[] dp) {
+        int res;
+        if(dp[n]!=0)
+            return dp[n];
+        if(num==n)
+            dp[n]=0;
+         else{
+             dp[n]=n;
+         }
+       
+        for(int i=1;i<n;i++)
+        {
+          int value=integerBreakRec(i,num,dp)*integerBreakRec(n-i,num,dp);
+          dp[n]=Math.max(dp[n],value);  
+        }
+        return dp[n];
+    }
+}
